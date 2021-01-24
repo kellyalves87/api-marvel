18bc70caa3c3896a68471e9d22e616f7d4d63746
Guilherme Garcia Alves <guilherme.garcia86@gmail.com>
2021-01-24 18:10:54 -0300

    Adicionado get Marvel API

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Thumbnail {
+    path: string;
+    extension: string;
+}
+
+interface Character {
+    id: string;
+    name: string;
+    thumbnail: Thumbnail;
+}
+
+interface MarvelResponseData {
+  results: Character[];
+};
+
+interface MarvelResponse {
+  data: MarvelResponseData
+}
+
+const defaultCharacters: Character[] = [];
+
+const App: React.FC = () => {
+
+    const [characters, setCharacters]: [Character[], (characters: Character[]) => void] = React.useState(defaultCharacters)
+
+    React.useEffect(() => {
+
+        axios.get<MarvelResponse>('https://gateway.marvel.com:443/v1/public/characters?apikey=', {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            timeout: 5000,
+        })
+        .then((response) => {
+          setCharacters(response.data.data.results)
+        })
+        .catch((ex) =>{
+            console.log('error: ', ex)
+        })
+
+    }, [])
+
+    console.log(characters)
+
+    return (
+        <>
+          <ul>
+              {characters.map((character) =>(
+                  <li key={character.id}>
+                      <h3>{character.name}</h3>
+                  </li>
+              ))}
+          </ul>
+        </>
+    )
+
 }
 
 export default App;
